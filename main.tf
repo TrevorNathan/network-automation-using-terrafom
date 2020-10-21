160aea8d00b3ed3939bd36c1ff03c13e831d7f1f
TrevorNathan <tech25trevor@gmail.com>
2020-10-22 00:53:50 +0300

    UPDATE: adding all in main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,76 @@
+
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
+## This code follows 0.12 syntax of HCL, its not compatible with any versions below 0.12.
+  required_version = ">= 0.12"
 }
 
 provider "google" {
   version = "3.5.0"
 
+
   credentials = file("account.json")
-
-  project = "mike-256617"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  
+  project     = var.var_project
+  region      = var.var_region
+  zone        = var.var_zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+# # ----------------------------------------------------------------
+# Create the mynetwork network
+# # --------------------------------------------------------------
+resource "google_compute_network" "my_network" {
+  name                    = "my_network"
+  auto_create_subnetworks = true
 }
 
-variable "instance_name" {}
-variable "instance_zone" {}
-variable "instance_type" {
-  default = "n1-standard-1"
+
+# # --------------------------------------------------------------
+# Add a firewall rule to allow HTTP, SSH, RDP, and ICMP traffic on mynetwork
+# # --------------------------------------------------------------
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+  name    = "my-compute-network-firewall"
+  description = "This allows http ssh rdp icmp"
+  network = google_compute_network.my_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
   }
-variable "instance_network" {}
 
+  allow {
+    protocol = "icmp"
+  }
+}
+
+# # -----------------------------------------------------------------------------------------
+// Creating compute Instance resource
+# # -----------------------------------------------------------------------------------------
 resource "google_compute_instance" "vm_instance" {
-  name         = "${var.instance_name}"
-  zone         = "${var.instance_zone}"
-  machine_type = "${var.instance_type}"
+  name                  = "my-vm-instance"
+  description           = "This is Johns vm"
+  machine_type          = var.var_machine_type
+  project               = var.var_project
+  zone                  = var.var_zone
+
+  tags             = ["tag-1", "tag-2"]
+
+  deletion_protection   = "false" 
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
-      }
-  }
-  network_interface {
-    network = "${var.instance_network}"
-    access_config {
-      # Allocate a one-to-one NAT IP to the instance
+      image      = "debian-cloud/debian-9"
     }
   }
+
+  network_interface {
+    network      = var.var_network
+
+    access_config {
+    nat_ip  = "" 
+     }
+
+  }
+
+
 }
+
--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
-variable "var"
+variable "var_project" {
+  default = "mike-256617"
+}
+
+variable "var_region" {
+  default = "us-central1"
+}
+
+variable "var_zone" {
+  default = "us-central1-c"
+}
+
+variable "var_machine_type" {
+  default = "n1-machine-n1"
+}
+
+variable "var_network" {
+  default = "my-network"
+}
